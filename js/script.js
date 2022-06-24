aa5403681774ee28bc7b4bb7b0ce5281b48cda23
Idogwu Emmanuel Chinonso <104998136+Nonnyjoe@users.noreply.github.com>
2022-06-24 04:38:44 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    responsive: {
+        340: {
+            items: 1.2
+        },
+        450: {
+            items: 1.5
+        },
+        502: {
+            items: 1.7
+        },
+        600: {
+            items: 1.8
+        },
+        768: {
+            items: 2.5
+        },
+        1000: {
+            items: 3.2
+        },
+        1200: {
+            items: 4
+        }
+    }
+});
